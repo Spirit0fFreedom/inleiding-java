bc5c0cb3be94ed8ed831168d4976a5c209cb9612
Spirit0fFreedom <www.royhuizing@hotmail.com>
2020-02-03 13:56:17 +0100

    Changed text

--- src/h01/Opdracht22.java
+++ src/h01/Opdracht22.java
@@ -1,16 +1,3 @@
-/*
-
-@(#)Show.java 1.0 03/02/26
-
-*
-
-Voorbeeld 2.1
-
-*
-
-*/
-
-//importeer de benodigde klassen uit de Java bibliotheek
 import javax.xml.soap.Text;
 import java.awt.*;
 import java.applet.*;
@@ -19,16 +6,21 @@ import java.awt.Label;
 
 
 //een klasse met de naam Show van het type Applet
-public class Show extends Applet {
+public class Opdracht22 extends Applet {
 
     //een (lege) methode die de Applet initialiseert
+    TextField text1,text2;
     public void init() {
-        setBackground(Color.blue);
+        setBackground(Color.white);
+
+
     }
     //een methode die de inhoud van het scherm tekent
     public void paint(Graphics g) {
-        g.setColor(Color.yellow);
-        g.drawString("Roy Huizing", 50, 60 );
+        g.setColor(Color.blue);
+        g.drawString("Roy", 50, 60 );
+        g.setColor(Color.red);
+        g.drawString("Huizing", 50, 75 );
     }
 
 }
