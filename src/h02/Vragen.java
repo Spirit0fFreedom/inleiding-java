0d315a9161deb8c7470bb50fa5b389595b94ceb7
Spirit0fFreedom <36427785+Spirit0fFreedom@users.noreply.github.com>
2020-10-08 00:45:46 +0200

    Update Vragen.java
    Removed excessive use of g.setColor();

--- src/h02/Vragen.java
+++ src/h02/Vragen.java
@@ -18,41 +18,31 @@ public class Vragen extends Applet {
     public void paint(Graphics g) {
         g.setColor(Color.white);
         g.drawString("Vragen", 50, 60 );
-        g.setColor(Color.white);
+
         g.drawString("Wat is machinetaal?", 50, 100 );
 
-        g.setColor(Color.white);
         g.drawString("Machinetaal is een taal die direct kan worden uitgevoerd door de processor." , 55, 120 );
-        g.setColor(Color.white);
         g.drawString("Als het goed is wordt in dit geval, JAVA omgezet in machinetaal door een compiler." , 55, 135 );
 
-        g.setColor(Color.white);
+
         g.drawString("Welke drie hulpmiddelen heb je nodig om te programmeren?", 50, 155 );
 
-        g.setColor(Color.white);
+
         g.drawString("Integrated Development Enviroment (IDE) programma,", 55, 175 );
-        g.setColor(Color.white);
         g.drawString("daar zitten meerdere functies bij inbegrepen, zoals een compiler, debugger en een editor.  En natuurlijk een toetsenboord haha!", 55, 190 );
 
-        g.setColor(Color.white);
         g.drawString("Hoe herken je opdrachten in de broncode?", 50, 210 );
 
-        g.setColor(Color.white);
         g.drawString("Die kun je herkennen omdat ze altijd moeten eindigen met een puntkomma ';'", 55, 227 );
 
-        g.setColor(Color.white);
         g.drawString("Waartoe dienen de import-opdrachten?';'", 50, 245 );
 
-        g.setColor(Color.white);
+
         g.drawString("Die opdrachten importeren een klassenbibliotheek waarin voorgedefinieerde codes te halen zijn waardoor je dus meer klassen kunt gebruik in je code';'", 55, 262 );
 
-        g.setColor(Color.white);
         g.drawString("Wat is een methode?", 50, 285 );
-        g.setColor(Color.white);
         g.drawString("Een methode is een stuk code die alleen werkt ls hij wordt opgeroepen.", 55, 303 );
-        g.setColor(Color.white);
         g.drawString("In een methode kun je ook data doorgeven, ook wel bekend als 'Parameters' ", 55, 316 );
-        g.setColor(Color.white);
         g.drawString("Methodes worden gebruikt om bepaalde acties uit te voeren, ze zijn ook bekend als functies.", 55, 329 );
     }
 
